14313ab5c6918e600b4ebcad96089392b4dfd5c8
Maxim Borodin <mborodin.dev@gmail.com>
2025-05-27 01:13:13 +0300

    Added minimal structure

--- createTone.ts
+++
@@ -1,9 +0,0 @@
-import { ToneFn, CreateToneOptions, Tone } from './types'
-
-export default function createTone(handler: ToneFn, options?: CreateToneOptions): Tone {
-  return {
-    name: options?.name ?? 'default',
-    handler,
-    subtone: options?.subtone,
-  }
-}
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export * from './types'
+
+export { createPalette } from './createPalette'
+export { createTone } from './createTone'
